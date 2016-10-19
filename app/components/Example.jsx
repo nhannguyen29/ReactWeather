6958275cfdab7422adde202f7d3690e9c3bc7c76
nhn29 <nhn29@drexel.edu>
2016-10-19 14:34:27 +0700

    Added paragraph

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -3,7 +3,8 @@ var React = require('react');
 var Example = (props) => {
     return (
         <div>
-            <h3>Example Component</h3>
+            <h3>Example</h3>
+            <p>Welcome to example page</p>
         </div>
         
     );
